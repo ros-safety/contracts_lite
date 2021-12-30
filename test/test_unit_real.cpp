42607e057e186697bdcba4dfe72d98c6992eb748
Jeffrey Kane Johnson <jeff@mapless.ai>
2021-12-30 09:19:20 -0500

    remove extra copyright

--- test/test_unit_real.cpp
+++ test/test_unit_real.cpp
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// \copyright Copyright 2021 Mapless AI, Inc.
-/// \file
-/// \brief This file tests the unit real type.
-
 #include <limits>
 
 #include "contracts_lite/simple_violation_handler.hpp"
